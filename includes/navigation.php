86e780fa523f34836e5f6e65fdab004ef212945a
Andrew <Andrew@andrews-MacBook-Pro.local>
2016-02-20 20:30:07 -0800

    Replaced logo
    changed to new logo

--- includes/navigation.php
+++ includes/navigation.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="img/img-logo-header.png" class='img-responsive' alt="" /></a>
+            <a class="navbar-brand" href="#"><img src="img/oc-logo.png" class='img-responsive' alt="" /></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
